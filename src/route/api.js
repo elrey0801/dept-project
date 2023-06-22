5a1148b9e56e19ea91b32eccf6e53f6818708c5f
elrey0801 <tungnguyen.dsc@gmail.com>
2023-06-22 14:23:00 +0700

    add auth to api

--- src/route/api.js
+++ src/route/api.js
@@ -5,21 +5,28 @@ import APIController from '../controller/APIController.js'
 let router = express.Router();
 
 const initAPIRoute = (app) => {
-    router.get('/get-group-detail/:group_id', APIController.getGroupDetail);
-    router.get('/get-date-detail/:date', APIController.getDateDetail);
-    router.get('/get-week-detail/:date', APIController.getWeekDetail);
-    router.get('/get-undefined-detail', APIController.getUndefinedDetail);
-    router.get('/get-usercreated-detail', APIController.getUserCreatedDetail);
-    router.get('/get-single-detail/:hidden_id', APIController.getSingleDetail);
-    router.post('/create-group-detail', APIController.createGroup);
-    router.put('/update-single', APIController.updateSingle); // PUT -> UPDATE
-    router.delete('/delete-single/:hidden_id', APIController.deleteSingle); // DELETE -> DELETE
+    router.get('/get-group-detail/:group_id', checkAuthenticated, APIController.getGroupDetail);
+    router.get('/get-date-detail/:date', checkAuthenticated, APIController.getDateDetail);
+    router.get('/get-week-detail/:date', checkAuthenticated, APIController.getWeekDetail);
+    router.get('/get-undefined-detail', checkAuthenticated, APIController.getUndefinedDetail);
+    router.get('/get-usercreated-detail', checkAuthenticated, APIController.getUserCreatedDetail);
+    router.get('/get-single-detail/:hidden_id', checkAuthenticated, APIController.getSingleDetail);
+    router.post('/create-group-detail', checkAuthenticated, APIController.createGroup);
+    router.put('/update-single', checkAuthenticated, APIController.updateSingle); // PUT -> UPDATE
+    router.delete('/delete-single/:hidden_id', checkAuthenticated, APIController.deleteSingle); // DELETE -> DELETE
 
-    router.get('/get-ptvh-status/:date', APIController.getPTVHStatus);
-    router.get('/get-single-note/:id', APIController.getSingleNote);
-    router.get('/get-ptvh-note/:date', APIController.getPTVHNote);
-    router.delete('/delete-single-note/:id', APIController.deleteSingleNote);
+    router.get('/get-ptvh-status/:date', checkAuthenticated, APIController.getPTVHStatus);
+    router.get('/get-single-note/:id', checkAuthenticated, APIController.getSingleNote);
+    router.get('/get-ptvh-note/:date', checkAuthenticated, APIController.getPTVHNote);
+    router.delete('/delete-single-note/:id', checkAuthenticated, APIController.deleteSingleNote);
     return app.use('/api/v1/', router);
+}
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
 }
 
 export default initAPIRoute;
